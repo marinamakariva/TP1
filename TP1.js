c4783f1ed08a2a395b571d9a6f50be40fb030480
Ana Maria <ana.morales.argota@gmail.com>
2025-09-24 12:22:48 -0300

    -TP1.js

--- TP1.js
+++ TP1.js
@@ -1,2 +1,13 @@
 'use strict';
+class Producto {
+  constructor(id, nombre, precio, stock) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+  }
 
+  mostrarInformacion() {
+    return `ID: ${this.id}, ${this.nombre}, Precio: $${this.precio}, Stock: ${this.stock}`;
+  }
+}
