072c08c88780629e2253bf00f76a7c126e3d0d4d
Marina <marina.makarova@davinci.edu.ar>
2025-09-24 23:31:03 -0300

    Agregué parcialmente las clases CarritoDeCompras y Cliente

--- TP1.js
+++ TP1.js
@@ -1,13 +1,54 @@
 'use strict';
-class Producto {
-  constructor(id, nombre, precio, stock) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.stock = stock;
-  }
 
-  mostrarInformacion() {
-    return `ID: ${this.id}, ${this.nombre}, Precio: $${this.precio}, Stock: ${this.stock}`;
-  }
+class Producto {
+
+    constructor(id, nombre, precio, stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    mostrarInformacion() {
+        return `ID: ${this.id}, ${this.nombre}, Precio: ${this.precio}, Stock: ${this.stock}`;
+    }
+}
+
+class CarritoDeCompras {
+
+    constructor(items = []) {
+        this.items = items;
+    }
+
+    agregarProducto(producto, cantidad) {
+        this.items.push({ producto, cantidad });
+    }
+    
+    removerProducto(producto) {
+        ///TODO
+    }
+
+    calcularTotal() {
+        ///TODO
+    }
+
+    mostrarCarrito() {
+        console.log('Contenido actual del carrito:');
+        for (const item of this.items) {
+            console.log(`${item.producto.mostrarInformacion()}, Cantidad: ${item.cantidad}`);
+        }
+    }
+}
+
+class Cliente {
+
+    constructor(nombre, direccionEnvio, carrito) {
+        this.nombre = nombre;
+        this.direccionEnvio = direccionEnvio;
+        this.carrito = carrito;
+    }
+    
+    realizarCompra() {
+        ///TODO
+    }
 }
